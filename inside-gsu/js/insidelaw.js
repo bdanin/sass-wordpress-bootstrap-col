9bcc92fedb35afae827c967ec0a85799169222eb
bdanin-cse <bdanin@groupcse.com>
2014-04-06 21:42:55 -0400

    comment

--- inside-gsu/js/insidelaw.js
+++ inside-gsu/js/insidelaw.js
@@ -1,5 +1,7 @@
 jQuery.noConflict();
 jQuery(document).ready(function($) {
+	
+	// add pop-up effect to mobile menu
 	$('.btn-activate').bind('click tap', function(e) {
 		e.preventDefault();    
     $('.menu-collapse').toggleClass('open-active');
