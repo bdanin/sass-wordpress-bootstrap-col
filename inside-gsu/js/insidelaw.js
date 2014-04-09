a637c15f929c596a3c45a6ae9e45411adfa56e73
bdanin-cse <bdanin@groupcse.com>
2014-04-08 23:08:43 -0400

    remove unused js

--- inside-gsu/js/insidelaw.js
+++ inside-gsu/js/insidelaw.js
@@ -1,16 +1,9 @@
 jQuery.noConflict();
 jQuery(document).ready(function($) {
-	
 	// add pop-up effect to mobile menu
 	$('.btn-activate').bind('click tap', function(e) {
 		e.preventDefault();    
     $('.menu-collapse').toggleClass('open-active');
     $(this).toggleClass('activated');
   });
-
-  // create toggle-states for 3rd tier mobile
-  // $('.menu-global li').bind('touchstart touchend', function(e) {
-  //   e.preventDefault();
-  //   $(this).toggleClass('hover_effect');
-  // });
 });
